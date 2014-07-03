e3ed40493b55b75e5c60e65236995872323accb2
Dat Do <DatDo@Dats-MacBook-Pro.local>
2014-07-03 13:32:40 -0400

    create brobot

---
+++ RobotWar.spritebuilder/Source/brobot.h
@@ -0,0 +1,13 @@
+//
+//  brobot.h
+//  RobotWar
+//
+//  Created by Dat Do on 7/3/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Robot.h"
+
+@interface brobot : Robot
+
+@end
---
+++ RobotWar.spritebuilder/Source/brobot.m
@@ -0,0 +1,13 @@
+//
+//  brobot.m
+//  RobotWar
+//
+//  Created by Dat Do on 7/3/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "brobot.h"
+
+@implementation brobot
+
+@end
