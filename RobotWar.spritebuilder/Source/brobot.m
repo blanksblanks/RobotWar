e656b189ecc7cde740be519fb2e47706560d9394
Dat Do <DatDo@Dats-MacBook-Pro.local>
2014-07-03 18:53:31 -0400

    add some random code for brobot

--- RobotWar.spritebuilder/Source/brobot.m
+++ RobotWar.spritebuilder/Source/brobot.m
@@ -6,8 +6,42 @@
 //  Copyright (c) 2014 Apportable. All rights reserved.
 //
 
+#import "MainScene.h"
 #import "brobot.h"
-
+typedef NS_ENUM(NSInteger, RobotState)
+{
+    RobotStateDefault,
+    RobotStateAim,
+    RobotStateSearch,
+    RobotStateFire,
+    RobotStateTurnandRun,
+    RobotStateRevenge,
+};
 @implementation brobot
+{
+    RobotState _currentRobotState;
+}
+- (void)run
+{
+    while (true) {
+    if (_currentRobotState == RobotStateFire) {
+        [self shoot];
+    }
+    }
+}
+-(void)scannedRobot:(Robot*)robot atPosition:(CGPoint)position{
+    [self moveAhead:70];
+    _currentRobotState = RobotStateFire;
+    CGFloat angle = [self angleBetweenGunHeadingDirectionAndWorldPosition:position];
+    if (angle >= 0){
+        [self turnGunRight:abs(angle)];
+    } else {
+    [self turnGunLeft:abs(angle)];
+    }
+    [self shoot];
+}
+-(void)bulletHitEnemy:(Bullet *)bullet{
+    
+}
 
 @end
